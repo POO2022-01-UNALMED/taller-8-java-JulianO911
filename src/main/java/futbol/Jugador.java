c3a7cff0a31b8cbd2bd133d80c922872d45cfc2e
Julian O <juospinam@unal.edu.co>
2022-05-18 16:42:39 -0500

    solucion taller 8

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,52 @@
+package futbol;
+
+public class Jugador extends Futbolista {
+	private short golesMarcados;
+	private byte dorsal;
+	public Jugador() {
+		this((short)289,(byte)7);
+	}
+	
+	public Jugador(String nombre,int edad,String posicion,short golesMarcados,byte dorsal) {
+		super(nombre,edad,posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	
+	public Jugador(short golesMarcados,byte dorsal) {
+		super();
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+
+    public short getGolesMarcados() {
+        return golesMarcados;
+    }
+
+    public void setGolesMarcados(short golesMarcados) {
+        this.golesMarcados = golesMarcados;
+    }
+
+    public byte getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(byte dorsal) {
+        this.dorsal = dorsal;
+    }
+	
+        
+	public int compareTo(Futbolista f) {
+		return this.getEdad() - f.getEdad();
+	}
+	
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+				" con el dorsal "+ dorsal + ". Ha marcado "+ golesMarcados;
+	}
+	
+	public boolean jugarConLasManos() {
+		return false;
+	}
+
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,41 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	private short golesRecibidos;
+	private byte dorsal;
+	public Portero(String nombre,int edad,short golesRecibidos,byte dorsal) {
+		super(nombre,edad,"Portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+	}
+
+    public short getGolesRecibidos() {
+        return golesRecibidos;
+    }
+
+    public void setGolesRecibidos(short golesRecibidos) {
+        this.golesRecibidos = golesRecibidos;
+    }
+
+    public byte getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(byte dorsal) {
+        this.dorsal = dorsal;
+    }
+	
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+				" con el dorsal "+ dorsal + ". Le han marcado "+ golesRecibidos;
+	}
+	
+	public int compareTo(Futbolista p) {
+		return this.golesRecibidos - ((Portero) p).golesRecibidos;
+	}
+	
+	public boolean jugarConLasManos() {
+		return true;
+	}
+
+}
